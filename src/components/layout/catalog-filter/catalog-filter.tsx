d446d25d109638f29f5a51023599f38d81659044
tequomo <tequomo@gmail.com>
2021-12-27 01:55:10 +0300

    ~

--- src/components/layout/catalog-filter/catalog-filter.tsx
+++ src/components/layout/catalog-filter/catalog-filter.tsx
@@ -1,4 +1,66 @@
+import { ChangeEvent, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getGuitars } from '../../../store/reducers/guitars-data/selectors';
+
+// type FilterParamsType = {
+//   filterByPrice: number[],
+//   filterByType: string,
+// }
+
+type ByPriceType = {
+  priceMin: number,
+  priceMax: number,
+}
+
 function CatalogFilter(): JSX.Element {
+
+  const guitars = useSelector(getGuitars);
+  const minPriceValue = Math.min(...guitars.map((guitar) => guitar.price));
+  const maxPriceValue = Math.max(...guitars.map((guitar) => guitar.price));
+
+  // const initFilterParams = {
+  //   filterByPrice: [minPriceValue, maxPriceValue],
+  //   filterByType: '',
+  // };
+
+  const initPriceParams: ByPriceType = {
+    priceMin: minPriceValue,
+    priceMax: maxPriceValue,
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // const [filterParams, setFilterParams] = useState<FilterParamsType>(initFilterParams);
+  const [filterByPrice, setFilterByPrice] = useState<ByPriceType>(initPriceParams);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [filterByType, setFilterByType] = useState<string>('');
+
+  // const handleMinPriceChange = (e: ChangeEvent<HTMLInputElement>): void => {
+  //   setFilterParams((state) => ({
+  //     ...state,
+  //     filterByPrice: [
+  //       +e.target.value,
+  //       ...rest,
+  //     ],
+  //   }));
+  // };
+
+  // eslint-disable-next-line no-console
+  // console.log(filterParams);
+
+  const handleMinPriceChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setFilterByPrice((state) => ({
+      ...state,
+      priceMin: +e.target.value,
+    }));
+  };
+
+  const handleMaxPriceChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    setFilterByPrice((state) => ({
+      ...state,
+      priceMax: +e.target.value,
+    }));
+  };
+
   return (
     <form className="catalog-filter">
       <h2 className="title title--bigger catalog-filter__title">Фильтр</h2>
@@ -7,11 +69,11 @@ function CatalogFilter(): JSX.Element {
         <div className="catalog-filter__price-range">
           <div className="form-input">
             <label className="visually-hidden">Минимальная цена</label>
-            <input type="number" placeholder="1 000" id="priceMin" name="от" />
+            <input type="number" placeholder={minPriceValue.toString()} id="priceMin" name="от" onChange={handleMinPriceChange} value={filterByPrice.priceMin}/>
           </div>
           <div className="form-input">
             <label className="visually-hidden">Максимальная цена</label>
-            <input type="number" placeholder="30 000" id="priceMax" name="до" />
+            <input type="number" placeholder={maxPriceValue.toString()} id="priceMax" name="до" onChange={handleMaxPriceChange} value={filterByPrice.priceMax}/>
           </div>
         </div>
       </fieldset>
